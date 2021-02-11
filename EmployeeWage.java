d434cc6d27310cb4255532bae09abf7b061de8c5
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-11 22:31:24 +0530

    Third Commit

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,15 +1,22 @@
 package com.blbz;
 
 public class EmployeeWage {
+	public static  final int  isPartTime = 1;
+	public static  final int isFullTime = 2;
+	public static  final int EMP_RATE_PER_HOUR=20;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int wagePerHour = 20;
-		int fullDayHour = 8;
-		float dailyWage;
+		int empHrs = 0;
+		int empWage=0;
+		double empCheck = Math.floor(Math.random()*10)%3;
 		
-		dailyWage = wagePerHour*fullDayHour;
-		System.out.println(dailyWage);
+		if(empCheck==isFullTime)
+				empHrs = 8;
+		else
+			empHrs = 4;
+		empWage = empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage : " + empWage);
 
 	}
 
