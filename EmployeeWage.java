019b2bd9597bd11e5305611fa253c62ce2e89e19
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-11 22:37:54 +0530

    Fourth Commit

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,20 +4,34 @@ public class EmployeeWage {
 	public static  final int  isPartTime = 1;
 	public static  final int isFullTime = 2;
 	public static  final int EMP_RATE_PER_HOUR=20;
-
+	public static  final int No_Of_Working_Days=2;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int empHrs = 0;
 		int empWage=0;
-		double empCheck = Math.floor(Math.random()*10)%3;
+		int totalEmpWage =0;
 		
-		if(empCheck==isFullTime)
-				empHrs = 8;
-		else
-			empHrs = 4;
-		empWage = empHrs*EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage : " + empWage);
+		for(int day = 1 ; day<=No_Of_Working_Days;day++ )
+		{
+			int empCheck = (int) Math.floor(Math.random()*10)%3;
+			
+			switch(empCheck)
+			{
+				case isFullTime:
+					empHrs=8;
+					break;
+				case isPartTime:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
+					break;
+			}
+			empWage=  empHrs*EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Emp Wage :"+ totalEmpWage);
 
 	}
-
+	}
 }
