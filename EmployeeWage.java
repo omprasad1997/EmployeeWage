b877ca0eaf82279a4ec1ca4c4c4da7041cf10a63
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-16 22:29:28 +0530

    Compute Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,11 +3,14 @@ package com.blbz;
 public class EmployeeWage {
 	public static  final int  isPartTime = 1;
 	public static  final int isFullTime = 2;
+	public static  final int EMP_RATE_PER_HOUR=20;
+	public static  final int No_Of_Working_Days=20;
+	public static  final int max_Hrs_In_Month = 100;
 	
-	public static int computeEmpWage(String company,int empRate,int numOfDays,int maxHrs)
+	public static int computeEmpWage()
 	{
 		int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
-		while(totalEmpHrs<=maxHrs && totalWorkingDays < numOfDays)
+		while(totalEmpHrs<=max_Hrs_In_Month && totalWorkingDays < No_Of_Working_Days)
 		{
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random()*10)%3;
@@ -28,12 +31,12 @@ public class EmployeeWage {
 			System.out.println("Days#: " + totalWorkingDays + " Emp Hr: " + empHrs);
 			
 	    }
-		int totalEmpWage = empHrs*empRate;
+		int totalEmpWage = empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage : "+ totalEmpWage);
 		return totalEmpWage;
 	}
 	
 	public static void main(String[] args) {
-		computeEmpWage("Jio",10,20,100);
+		computeEmpWage();
 	}
 }
